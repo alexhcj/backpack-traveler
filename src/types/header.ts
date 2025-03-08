47ba2705e100030655d9c084f4d75b5d1f0dc658
@alex_hcj <alexhcj@yandex.ru>
2025-03-08 07:57:28 +0300

    add: header types description

--- src/types/header.ts
+++ src/types/header.ts
@@ -1,6 +1,6 @@
 export enum EHeader {
-  BASE = "base",
-  BASE_COMPACT = "base-compact",
-  TRANSPARENT = "transparent",
-  CENTERED = "centered",
+  BASE = "base", // (top + bot (menu), prime backg) - dest | nav | social + search
+  BASE_COMPACT = "base-compact", // (top + bot (no menu), prime backg) - dest | nav | social + search
+  TRANSPARENT = "transparent", // (top, transparent backg) - logo | nav | social + search. TODO: add socials + search
+  CENTERED = "centered", // (top, transparent back) - dest | nav + logo | social + search
 }
