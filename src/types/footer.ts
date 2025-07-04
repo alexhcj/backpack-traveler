9927828d3f7563633f6fb0c8303325bd1e5620ef
@alex_hcj <alexhcj@yandex.ru>
2025-07-04 19:25:17 +0300

    extend: footer types, add to layouts, add header type to 404 page

--- src/types/footer.ts
+++ src/types/footer.ts
@@ -1,3 +1,5 @@
 export enum EFooter {
   FULL = "full", // with top block: world map, socials & newsletter
+  SHORT_BORDER = "short-border", // only bottom with padding top + border
+  SHORT = "short", // only bottom
 }
